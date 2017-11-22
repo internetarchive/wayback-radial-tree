0d1c43492285c9aa03e58a4883ec0222f1d9e4f0
Eagle19243 <eagle19243@gmail.com>
2017-11-22 18:05:02 +0200

    # C10 - Radial Tree Library initial Prototype

--- build/js/radialTree.js
+++ build/js/radialTree.js
@@ -1,5 +1,46 @@
 var libraryName = "RadialTree";
 
-function RadialTree(element, options){
+/**
+ * Radial Tree Library
+ * 
+ * @param {Object} element
+ */
+function RadialTree(element){
+    var defaults = {
+
+    }
+
+    this.element = element;
+    console.log("Radial Tree init-");
+    console.log("Element-", element);
+    this.onShown = this.onShown.bind(this);
+    this.onHidden = this.onHidden.bind(this);
+}
+
+/**
+ * Show Radial Tree element
+ */
+RadialTree.prototype.show = function(){
+
+}
+
+/**
+ * Hide Radial Tree element
+ */
+RadialTree.prototype.hide = function(){
+
+}
+
+/**
+ * On Shown event 
+ */
+RadialTree.prototype.onShown = function(event){
     
+}
+
+/**
+ * On Hidden event
+ */
+RadialTree.prototype.onHidden = function(event){
+
 }
--- src/js/radialTree.js
+++ src/js/radialTree.js
@@ -1,5 +1,44 @@
 var libraryName = "RadialTree";
 
-function RadialTree(element, options){
+/**
+ * Radial Tree Library
+ * 
+ * @param {Object} option
+ */
+function RadialTree(option){
+    var defaults = {
+
+    }
+
+    this.option = option;
+    this.onShown = this.onShown.bind(this);
+    this.onHidden = this.onHidden.bind(this);
+}
+
+/**
+ * Show Radial Tree element
+ */
+RadialTree.prototype.show = function(){
+
+}
+
+/**
+ * Hide Radial Tree element
+ */
+RadialTree.prototype.hide = function(){
+
+}
+
+/**
+ * On Shown event 
+ */
+RadialTree.prototype.onShown = function(event){
     
+}
+
+/**
+ * On Hidden event
+ */
+RadialTree.prototype.onHidden = function(event){
+
 }

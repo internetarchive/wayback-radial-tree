63f57962790e0a2911801c6c5b877e17525feb5b
Eagle19243 <eagle19243@gmail.com>
2017-11-23 22:04:03 +0200

    # C12

--- src/js/radialTree.js
+++ src/js/radialTree.js
@@ -8,9 +8,9 @@ function RadialTree(element, option){
     this.element = element;
     this.option = option;
 
-    init(element, option);
+    Init(element, option);
 
-    function init(container, option){
+    function Init(container, option){
         var content = document.createElement("div");
         content.setAttribute("class", "radial-tree-content");
         var span = document.createElement("button");
@@ -39,6 +39,64 @@ function RadialTree(element, option){
 
         span.onclick = function(){
             container.style.display = "none";
+        }
+    }
+
+    function GetData(url, cb){
+        var RequestURL = "https://web.archive.org/web/timemap/json?" + 
+            "url=" + url + "/&" + 
+            "fl=timestamp:4,original,urlkey&" + 
+            "matchType=prefix&" + 
+            "filter=statuscode:200&" + 
+            "filter=mimetype:text/html&" + 
+            "collapse=urlkey&" + 
+            "collapse=timestamp:4";
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", RequestURL, true);
+		xhr.onerror = function(){
+            cb(false, "An error occured. Please refresh the page and try again");
+		};
+		xhr.ontimeout = function(){
+			cb(false, "Timeout, Please refresh the page and try again");
+        }
+        xhr.onload = function(){
+            var response = JSON.parse(xhr.responseText);
+            if (response.length == 0) cb(true, []);
+            
+            var array_Year = new Array();
+            var index_Year = 0;
+
+            for (var i=1; i<response.length; i++) {
+                if (response[i][1].match(/jpg|pdf|png|form|gif/)) {
+                    continue;
+                }
+
+                response[i][1] = (function(){
+                    var tmpAry = response[i][2].split(",");
+                    var domain = "";
+                    var url = "";
+    
+                    for (var k=0; k<tmpAry.length-1; k++) {
+                        if (k == 0) {
+                            domain = tmpAry[0];
+                        } else {
+                            domain = tmpAry[k] + "." + domain;
+                        } 
+                    }
+
+                    return tmpAry[tmpAry.length - 1].slice(-1) == "/" ? 
+                        "http://www." + tmpAry[tmpAry.length - 1].replace(")/", "." + domain + "/") :
+                        "http://www." + url + "/" + tmpAry[tmpAry.length - 1].replace(")/", "." + domain + "/");
+                }());
+
+                if (i == 1) {
+                    array_Year[0] = new Array();
+                } else if (response[i-1][1] == response[i][1]) {
+
+                } else {
+                    index_Year ++;
+                }
+            }
         }
     }
 }

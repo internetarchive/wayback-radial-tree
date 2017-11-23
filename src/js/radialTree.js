4b48887a932e7bf8485f480b9a5dc0842cd8d672
Eagle19243 <eagle19243@gmail.com>
2017-11-23 23:53:04 +0200

    # C13

--- src/js/radialTree.js
+++ src/js/radialTree.js
@@ -63,40 +63,101 @@ function RadialTree(element, option){
             var response = JSON.parse(xhr.responseText);
             if (response.length == 0) cb(true, []);
             
-            var array_Year = new Array();
-            var index_Year = 0;
-
-            for (var i=1; i<response.length; i++) {
-                if (response[i][1].match(/jpg|pdf|png|form|gif/)) {
-                    continue;
-                }
-
-                response[i][1] = (function(){
-                    var tmpAry = response[i][2].split(",");
-                    var domain = "";
-                    var url = "";
-    
-                    for (var k=0; k<tmpAry.length-1; k++) {
-                        if (k == 0) {
-                            domain = tmpAry[0];
-                        } else {
-                            domain = tmpAry[k] + "." + domain;
-                        } 
+            var array_Year = (function(){
+                var ret = new Array();
+                var index_Year = 0;
+                for (var i=1; i<response.length; i++) {
+                    if (response[i][1].match(/jpg|pdf|png|form|gif/)) {
+                        continue;
                     }
-
-                    return tmpAry[tmpAry.length - 1].slice(-1) == "/" ? 
-                        "http://www." + tmpAry[tmpAry.length - 1].replace(")/", "." + domain + "/") :
-                        "http://www." + url + "/" + tmpAry[tmpAry.length - 1].replace(")/", "." + domain + "/");
-                }());
-
-                if (i == 1) {
-                    array_Year[0] = new Array();
-                } else if (response[i-1][1] == response[i][1]) {
-
-                } else {
-                    index_Year ++;
+    
+                    response[i][1] = (function(){
+                        var tmpAry = response[i][2].split(",");
+                        var domain = "";
+                        var url = "";
+        
+                        for (var k=0; k<tmpAry.length-1; k++) {
+                            if (k == 0) {
+                                domain = tmpAry[0];
+                            } else {
+                                domain = tmpAry[k] + "." + domain;
+                            } 
+                        }
+    
+                        return tmpAry[tmpAry.length - 1].slice(-1) == "/" ? 
+                            "http://www." + tmpAry[tmpAry.length - 1].replace(")/", "." + domain + "/") :
+                            "http://www." + url + "/" + tmpAry[tmpAry.length - 1].replace(")/", "." + domain + "/");
+                    }());
+    
+                    if (i == 1) {
+                        ret[0] = new Array();
+                        ret[0].push(response[1][1]);
+                        ret[0].push(response[1][0]);
+                    } else if (response[i-1][1] == response[i][1]) {
+                        ret[index_Year].push(response[i][0]);
+                    } else {
+                        index_Year ++;
+                        ret[index_Year] = new Array();
+                        ret[index_Year].push(response[i][1]);
+                        ret[index_Year].push(response[i][0]);
+                    }
                 }
-            }
+
+                return ret;
+            }());
+
+            var years = (function(){
+                var ret = new Array();
+                for (var i=1; i<array_Year[0].length; i++) {
+                    ret[i-1] = new Array();
+                    ret[i-1].push(array_Year[0][i]);
+                }
+                for (var i=0; i<array_Year.length; i++) {
+                    var url = array_Year[i][0];
+                    for (var j=1; j<array_Year[i].length; j++) {
+                        var date = array_Year[i][j];
+                        var k = 0; 
+                        if (ret[k] != undefined) {
+                            while(ret[k] != undefined && ret[k][0] != date) {
+                                k++;
+                            }
+                            if (ret[k] != undefined) {
+                                ret[k].push(url);
+                            }
+                        }
+                    }
+                }
+                for (var i=0; i<ret.length; i++) {
+                    for (var j=1; j<ret[i].length; j++) {
+                        var url;
+                        if (ret[i][j].includes("http")) { 
+                            url = ret[i][j].substring(7);
+                        } else if (ret[i][j].includes("https")) {
+                            url = ret[i][j].sugstring(8);
+                        }
+                        url = url.slice(0, -1);
+                        if (url.includes('//')) {
+                            url = url.split('//').join('/');
+                        }
+                        url = url.split('/').join('/');
+                        ret[i][j] = url;
+                    }
+                }
+
+                return ret;
+            }());
+            
+            var all_years = (function(){
+                var ret = new Array();
+                for (var i = 0; i < years.length; i++) {
+                    if (years[i].length > 1) {
+                        ret.push(years[i][0]);
+                    }
+                }
+
+                return ret;
+            }());
+            
         }
     }
 }

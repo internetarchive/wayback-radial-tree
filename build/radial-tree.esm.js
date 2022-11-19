714b84c982b4fe45e653073df4b02337847360c5
Vangelis Banos <vangelis@archive.org>
2022-11-19 20:18:20 +0000

    Update build files

--- build/radial-tree.esm.js
+++ build/radial-tree.esm.js
@@ -1,6 +1,54 @@
 import * as d3 from 'd3';
 import _ from 'lodash';
 
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  Object.defineProperty(Constructor, "prototype", {
+    writable: false
+  });
+  return Constructor;
+}
+function _toArray(arr) {
+  return _arrayWithHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableRest();
+}
+function _arrayWithHoles(arr) {
+  if (Array.isArray(arr)) return arr;
+}
+function _iterableToArray(iter) {
+  if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);
+}
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+  return arr2;
+}
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+
 /**
  * get SURT (Sort-friendly URI Reordering Transform)
  * https://github.com/internetarchive/surt
@@ -15,38 +63,10 @@ function surtToUrl(surt) {
   if (!surt) {
     return surt;
   }
-  //drop last ')'
+  // drop last ')'
   surt = surt.slice(0, surt.length - 1);
   return surt.split(',').reverse().join('.');
 }
-
-var classCallCheck = function (instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-};
-
-var createClass = function () {
-  function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
-    }
-  }
-
-  return function (Constructor, protoProps, staticProps) {
-    if (protoProps) defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) defineProperties(Constructor, staticProps);
-    return Constructor;
-  };
-}();
-
-var toArray = function (arr) {
-  return Array.isArray(arr) ? arr : Array.from(arr);
-};
 
 /**
  * @private
@@ -61,17 +81,13 @@ function buildHierarchInDepth(parent, path) {
   if (!path || path.length === 0) {
     return;
   }
-
-  var _path = toArray(path),
-      name = _path[0],
-      rest = _path.slice(1);
-
+  var _path = _toArray(path),
+    name = _path[0],
+    rest = _path.slice(1);
   if (!name) {
     return;
   }
-
-  var nextParent = void 0;
-
+  var nextParent;
   if (parent.children) {
     nextParent = parent.children.filter(function (c) {
       return c.name === name;
@@ -79,15 +95,12 @@ function buildHierarchInDepth(parent, path) {
   } else {
     parent.children = [];
   }
-
   if (!nextParent) {
     nextParent = {
       name: name
     };
-
     parent.children.push(nextParent);
   }
-
   buildHierarchInDepth(nextParent, rest);
 }
 
@@ -107,15 +120,12 @@ function buildHierarchInDepth(parent, path) {
  */
 function buildHierarchy(fields, data, _ref) {
   var targetField = _ref.targetField;
-
   return data.reduce(function (res, row) {
     var value = fields.getValueByName(row, targetField);
-
     var _value$split = value.split('/'),
-        _value$split2 = toArray(_value$split),
-        host = _value$split2[0],
-        path = _value$split2.slice(1);
-
+      _value$split2 = _toArray(_value$split),
+      host = _value$split2[0],
+      path = _value$split2.slice(1);
     res.name = surtToUrl(host);
     buildHierarchInDepth(res, path);
     return res;
@@ -126,10 +136,9 @@ function buildHierarchy(fields, data, _ref) {
  * extract fields from time map data
  *
  */
-var Fields = function () {
+var Fields = /*#__PURE__*/function () {
   function Fields(data) {
-    classCallCheck(this, Fields);
-
+    _classCallCheck(this, Fields);
     this.fields = data[0];
     this.getIndexByName = _.memoize(this.getIndexByName);
   }
@@ -139,10 +148,8 @@ var Fields = function () {
    *
    * @param name
    */
-
-
-  createClass(Fields, [{
-    key: 'getIndexByName',
+  _createClass(Fields, [{
+    key: "getIndexByName",
     value: function getIndexByName(name) {
       return this.fields.indexOf(name);
     }
@@ -154,9 +161,8 @@ var Fields = function () {
      * @param name
      * @returns {*}
      */
-
   }, {
-    key: 'getValueByName',
+    key: "getValueByName",
     value: function getValueByName(row, name) {
       return row[this.getIndexByName(name)];
     }
@@ -174,7 +180,6 @@ function extractYearsFromGroupedTimeMap(data) {
   if (!data) {
     return data;
   }
-
   return Object.keys(data).sort();
 }
 
@@ -196,16 +201,13 @@ function extractYearsFromGroupedTimeMap(data) {
  */
 function processTimeMap(data) {
   var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-      groupBy = _ref.groupBy,
-      dedupBy = _ref.dedupBy,
-      orderBy = _ref.orderBy;
-
+    groupBy = _ref.groupBy,
+    dedupBy = _ref.dedupBy,
+    orderBy = _ref.orderBy;
   if (!data) {
     return data;
   }
-
   var fields = new Fields(data);
-
   var res = data.slice(1).reduce(function (result, row) {
     var oneGroup = result[fields.getValueByName(row, groupBy)] || {};
 
@@ -213,7 +215,6 @@ function processTimeMap(data) {
     if (!oneGroup[fields.getValueByName(row, dedupBy)]) {
       oneGroup[fields.getValueByName(row, dedupBy)] = row;
     }
-
     result[fields.getValueByName(row, groupBy)] = oneGroup;
     return result;
   }, {});
@@ -232,7 +233,6 @@ function renderContainer() {
   content.setAttribute('class', 'rt-content');
   var divBtn = document.createElement('div');
   divBtn.setAttribute('class', 'div-btn');
-
   var sequence = document.createElement('p');
   sequence.setAttribute('class', 'sequence');
   var chart = document.createElement('div');
@@ -253,7 +253,6 @@ var arc = d3.arc().startAngle(function (d) {
 }).outerRadius(function (d) {
   return Math.sqrt(d.y1);
 });
-
 var colors = d3.scaleOrdinal(d3.schemePaired);
 
 /**
@@ -269,21 +268,20 @@ var colors = d3.scaleOrdinal(d3.schemePaired);
 function createVisualization(element, vis, radius, baseURL, currentYear, data) {
   var partition = d3.partition().size([2 * Math.PI, radius * radius]);
 
-  //append 'root' we will exclude it on rendering
-  var root = d3.hierarchy({ children: [data] }).sum(function (d) {
+  // append 'root' we will exclude it on rendering
+  var root = d3.hierarchy({
+    children: [data]
+  }).sum(function (d) {
     return !d.children;
   }).sort(function (a, b) {
     return b.value - a.value;
   });
-
   var nodes = partition(root).descendants();
-
   vis.selectAll('path').data(nodes).enter().append('a').attr('xlink:href', currentUrl).on('touchstart', touchStart).append('svg:path').attr('display', function (d) {
     return d.depth ? null : 'none';
   }).attr('d', arc).attr('fill-rule', 'evenodd').style('fill', function (d) {
     return colors((d.children ? d : d.parent).data.name);
   }).style('opacity', 1).style('cursor', 'pointer').on('mouseover', mouseover);
-
   d3.select('#d3_container').on('mouseleave', mouseleave);
 
   /** on mobile devices, touching the RadialTree prevents the ``click``
@@ -295,8 +293,8 @@ function createVisualization(element, vis, radius, baseURL, currentYear, data) {
     mouseover(d);
     return false;
   }
-
   function currentUrl(d) {
+    // TODO skip the reverse to speed it up.
     var anc = d.ancestors().reverse();
     var url = '';
     for (var i = 1; i < anc.length; i++) {
@@ -305,31 +303,25 @@ function createVisualization(element, vis, radius, baseURL, currentYear, data) {
       }
       url = url + '/' + anc[i].data.name;
     }
-    return baseURL + '/web/' + currentYear + '0630' + url;
+    return "".concat(baseURL, "/web/").concat(currentYear, "0630").concat(url);
   }
-
   function mouseover(d) {
     var sequenceArray = d.ancestors().reverse();
     sequenceArray.shift();
     var url = currentUrl(d);
     updateBreadcrumbs(sequenceArray, url);
     d3.selectAll('path').style('opacity', 0.3);
-
     vis.selectAll('path').filter(function (node) {
       return sequenceArray.indexOf(node) >= 0;
     }).style('opacity', 1);
   }
-
   function mouseleave() {
     element.querySelector('.sequence').innerHTML = '';
-
     d3.selectAll('path').on('mouseover', null);
-
     d3.selectAll('path').transition().style('opacity', 1).on('end', function () {
       d3.select(this).on('mouseover', mouseover);
     });
   }
-
   function updateBreadcrumbs(nodeArray, url) {
     var text = '';
     var symb = document.createElement('span');
@@ -343,7 +335,7 @@ function createVisualization(element, vis, radius, baseURL, currentYear, data) {
       }
     }
     text = decodeURIComponent(text);
-    element.querySelector('.sequence').innerHTML = '<a href="' + url + '">' + text + '</a>';
+    element.querySelector('.sequence').innerHTML = "<a href=\"".concat(url, "\">").concat(text, "</a>");
   }
 }
 
@@ -354,11 +346,9 @@ function renderYearButton(year) {
   btn.innerHTML = year;
   return btn;
 }
-
 function getYearByBtn(btn) {
   return btn.id;
 }
-
 function getButtonByYear(container, year) {
   return container.ownerDocument.getElementById(year);
 }
@@ -368,7 +358,6 @@ function renderYearButtons(element, allYears, onYearSelect) {
   divBtn.onclick = function (evt) {
     return onYearSelect(getYearByBtn(evt.target));
   };
-
   if (!element.querySelector('.year-btn')) {
     allYears.forEach(function (year) {
       return divBtn.appendChild(renderYearButton(year));
@@ -377,11 +366,10 @@ function renderYearButtons(element, allYears, onYearSelect) {
 }
 
 /**
- *
  * Radial Tree Library
  *
  * @param {DOMElement} element
- * @param {Array} cdx_data: decoded CDX Query data retrieved by:
+ * @param {Array} cdxData: decoded CDX Query data retrieved by:
  ``/web/timemap/json?url=example.com/&fl=timestamp:4,urlkey&matchType=prefix
  &filter=statuscode:200&filter=mimetype:text/html&collapse=urlkey
  &collapse=timestamp:4&limit=100000``.
@@ -389,24 +377,20 @@ function renderYearButtons(element, allYears, onYearSelect) {
  * Option baseURL defines the target Wayback Machine server.
  *
  */
-function RadialTree(element, cdx_data) {
+function RadialTree(element, cdxData) {
   var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-
   var baseURL = options.baseURL || 'https://web.archive.org';
 
   // render
-
   var container = renderContainer();
   element.appendChild(container);
-
-  var fields = new Fields(cdx_data);
-  var urlsByYear = processTimeMap(cdx_data, {
+  var fields = new Fields(cdxData);
+  var urlsByYear = processTimeMap(cdxData, {
     groupBy: 'timestamp:4',
     dedupBy: 'urlkey',
     orderBy: 'urlkey'
   });
   var years = extractYearsFromGroupedTimeMap(urlsByYear);
-
   renderYearButtons(element, years, selectYear);
 
   // highlight the 2nd last year if available, else hightlight the last.
@@ -414,7 +398,6 @@ function RadialTree(element, cdx_data) {
   // const lastButOneYear = allYears[allYears.length - 2] || allYears[0];
   var selectedBtn = years[years.length - 2] || years[0];
   selectYear(selectedBtn);
-
   function selectYear(year) {
     // hide active button
     if (element.querySelector('.active-btn')) {
@@ -426,27 +409,20 @@ function RadialTree(element, cdx_data) {
     if (btn) {
       btn.classList.add('active-btn');
     }
-
     renderChart(element, year);
   }
-
   function renderChart(element, currentYear) {
     element.querySelector('.sequence').innerHTML = '';
     element.querySelector('#chart').innerHTML = '';
-
     var width = element.querySelector('#chart').offsetWidth;
     var height = width;
     var radius = Math.min(width, height) / 2;
-
     var vis = d3.select('#chart').append('svg:svg').attr('width', width).attr('height', height).append('svg:g').attr('id', 'd3_container').attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-
     vis.append('svg:circle').attr('r', radius).style('opacity', 0);
-
     var urls = urlsByYear[currentYear];
     var hierarchy = buildHierarchy(fields, urls, {
       targetField: 'urlkey'
     });
-
     createVisualization(element, vis, radius, baseURL, currentYear, hierarchy);
   }
 }
